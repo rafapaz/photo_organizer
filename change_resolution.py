53777c4698883ae81359a513a187f8266ee4033c
rafapaz <rafapaz@gmail.com>
2019-11-20 22:24:48 -0300

    script to decrease resolution of all photos in a folder

---
+++ change_resolution.py
@@ -0,0 +1,50 @@
+import os
+from datetime import datetime
+import shutil
+from PIL import Image
+import argparse
+
+
+def main(args):
+    PATH = args.image_folder    
+    PATH_RESIZED = PATH + '\\resized'
+    MAXWIDTH = args.new_width
+    EXTENSIONS = ['.jpg', '.jpeg', '.tif', '.tiff', '.bmp', '.png', '.gif']
+
+    if os.path.isdir(PATH_RESIZED):
+        shutil.rmtree(PATH_RESIZED)
+    
+    shutil.copytree(PATH, PATH_RESIZED)
+
+    files = []
+    # r=root, d=directories, f = files
+    for r, d, f in os.walk(PATH_RESIZED):
+        for file in f:            
+            files.append(os.path.join(r, file))
+
+    for f in files:        
+        try:
+            ex = f[f.index('.'):].lower()
+            if ex not in EXTENSIONS:
+                os.remove(f)
+                continue
+                        
+            img = Image.open(f)
+            width = img.size[0]
+            if width <= MAXWIDTH:
+                continue
+            height = img.size[1]
+            ratio = (MAXWIDTH/float(width))
+            hsize = int((float(height)*float(ratio)))
+            img = img.resize((MAXWIDTH,hsize), Image.ANTIALIAS)            
+            img.save(f)
+        except Exception:
+            print('Error trying to resize image {}.'.format(f))        
+        
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Decrease resolution of all photos in folder, if width is greater than new width. It creates a folder "resized" with the new photos.')
+    parser.add_argument('image_folder', metavar='path', type=str, help='The image folder path')
+    parser.add_argument('new_width', metavar='width', type=int, help='The new max width')
+    args = parser.parse_args()
+    main(args)
